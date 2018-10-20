9a33e1b8f00d8bc3e0dd93eccb16a2027edddce5
Ivar Mahhonin <ivar.mahhonin@gmail.com>
2018-10-21 00:30:32 +0300

    Added docker nature. Added simple web server

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,5 +5,7 @@ object Dependencies {
   val telegramCore = "com.bot4s" %% "telegram-core" % "4.0.0-RC1"
   val telegramAkka = "com.bot4s" %% "telegram-akka" % "4.0.0-RC1"
   val lightbend = "com.typesafe" % "config" % "1.3.2"
-  val botDependencies: Seq[sbt.ModuleID] = Seq(scalaTest % Test, telegramAkka, telegramCore, lightbend)
+  val akkaHttp = "com.typesafe.akka" %% "akka-http" % "10.1.5"
+  val akka = "com.typesafe.akka" %% "akka-stream" % "2.5.12"
+  val botDependencies: Seq[sbt.ModuleID] = Seq(scalaTest % Test, telegramAkka, telegramCore, lightbend, akkaHttp, akka)
 }
--- src/main/scala/main/Main.scala
+++ src/main/scala/main/Main.scala
@@ -1,24 +1,27 @@
 import java.util
+import akka.actor.ActorSystem
+import akka.stream.ActorMaterializer
 import bot.Bot
 import com.typesafe.config.ConfigFactory
 
 object Main {
   def main(args: Array[String]) {
-    try {
-      val conf = ConfigFactory.load()
-      val token: String = conf.getString("bot-app.token")
-      val users: util.List[Integer] = conf.getIntList("bot-app.users")
-      val imageSource: String = conf.getString("bot-app.image-source")
-      val bot = new Bot(token, users, imageSource)
-      bot.run()
+    implicit val system = ActorSystem("my-system")
+    implicit val materializer = ActorMaterializer()
+    implicit val executionContext = system.dispatcher
 
-      println("Press [ENTER] to shutdown the bot, it may take a few seconds...")
+    val conf = ConfigFactory.load()
+    val token: String = conf.getString("bot-app.token")
+    val users: util.List[Integer] = conf.getIntList("bot-app.users")
+    val imageSource: String = conf.getString("bot-app.image-source")
+    val bot = new Bot(token, users, imageSource)
+    bot.run()
 
-      scala.io.StdIn.readLine()
+    println(s"Bot is running.")
+
+    system.registerOnTermination(() => {
+      println("Shutting down bot.")
       bot.shutdown()
-    }
-    catch {
-      case _: Throwable => println("Is your 'settings.conf' file there?")
-    }
+    })
   }
 }
